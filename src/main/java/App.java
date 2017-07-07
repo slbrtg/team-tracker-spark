eaf6f053e22b56597e728b358db092c36e04c4d0
Saul Artiga <slbrtg@gmail.com>
2017-07-07 11:37:35 -0700

    create initial files and folders

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+
+  public static void main(String[] args) {
+  }
+}
